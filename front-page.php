0287aa7f26f8223ef3b81bc2ab7c2527e5488712
Matt Beall <mbeall@starverte.com>
2013-06-28 12:44:41 -0600

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying the front page
+ *
+ * Template file used to render the Site Front Page,
+ * whether the front page displays the Blog Posts Index
+ * or a static page.
+ *
+ * @package Flint
+ *
+ */
+
+get_template_part( 'header', 'front' ); ?>
+
+		<div id="primary" class="content-area span8">
+			
+    	<div class="latest-posts">
+      	<h2>Recent Posts</h2>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>><a href="<?php the_permalink(); ?>"><h3 class="entry-title"><?php the_title(); ?></h3></a><p><?php flint_the_content('Read More', false, 'btn btn-success'); ?></p></article><!-- #post-<?php the_ID(); ?> -->
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php flint_content_nav( 'nav-below' ); ?>
+        
+      </div><!-- .latest-posts -->
+      
+      
+        
+		</div><!-- #secondary -->
+    
+    <div id="tertiary" class="span4">
+    
+    	<div class="other-sites">
+      	<h2>Other Sites</h2>
+        
+        <div class="row-fluid">
+        	<div class="span2"><a href="http://starverte.com/sparks"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/sparks.png" alt="Sparks Logo"></a></div>
+          <div class="span10">
+          	<a class="link-sparks" href="http://starverte.com/sparks"><h4>Sparks</h4></a>
+            <p>Powerful WordPress development framework</p>
+          </div>
+        </div>
+
+				<div class="row-fluid">
+          <div class="span12">
+          	<a class="link-fcc" href="http://fortcollinscreative.com"><h4>Fort Collins Creative</h4></a>
+            <p>Easy and intuitive WordPress hosting, maintenance, and support</p>
+          </div>
+        </div>
+      </div>
+        
+      <div class="twitter"></div>
+      	<a class="twitter-timeline" href="https://twitter.com/starverte" data-widget-id="349574290993188865">Tweets by @starverte</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div><!-- .twitter -->
+
+		</div><!-- #tertiary .other-sites -->
+	
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Flint
+ */
+?><!DOCTYPE html>
+<!--[if lt IE 9]><html <?php language_attributes(); ?> class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php $sparks_options = get_option('sparks_options');
+
+if (isset($sparks_options['fb_app_id'])) {
+	$fb_app_id = $sparks_options["fb_app_id"];
+}
+else {
+	$fb_app_id = '';
+} ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?php echo $fb_app_id; ?>";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+  
+  	<nav role="navigation" class="navbar navbar-inverse">
+			<h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
+			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
+			<div class="navbar-inner">
+				<div class="container">
+                
+					<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+                    
+					<!-- Be sure to leave the brand out there if you want it shown -->
+					<a class="brand" href="<?php echo home_url(); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
+                    
+					<div class="nav-collapse collapse">
+						<?php wp_nav_menu( array( 'menu_class' => 'nav', 'container' => false, 'theme_location' => 'primary', 'walker' => new Flint_Bootstrap_Menu ) ); ?>
+						<form method="get" class="navbar-search pull-right visible-desktop" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+							<input type="text" class="search-query" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="Search">
+						</form>
+					</div><!-- .nav-collapse -->
+				</div><!-- .container -->
+			</div><!-- .navbar-inner -->
+  	</nav><!-- .navbar -->
+
+	</header><!-- #masthead -->
+  
+<div id="page" class="hfeed site container">
+
+	<div id="main" class="site-main row-fluid">
