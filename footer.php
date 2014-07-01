9a39c94f8f355049b537680bfde533dc800a2fac
Matt Beall <mbeall@starverte.com>
2014-07-01 14:30:57 -0600

    Clean up spacing

--- footer.php
+++ footer.php
@@ -8,16 +8,16 @@
  */
 ?>
 
-	</div><!-- #main -->
+  </div><!-- #main -->
 
-	<footer id="colophon" class="site-footer container" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'flint_credits' ); ?>
-			Proudly powered by <a href="http://wordpress.org/" rel="generator" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a> and the <a href="http://sparks.starverte.com/" rel="generator" title="<?php esc_attr_e( 'A WordPress Development Framework', 'flint' ); ?>">Sparks Framework</a>
-			<span class="sep"> | </span>
-			<?php $theme = wp_get_theme(); printf( __( 'Theme: %1$s by %2$s.', 'flint' ), $theme, '<a href="'.$theme->{'Author URI'}.'" rel="designer">'.$theme->{'Author'}.'</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer container" role="contentinfo">
+    <div class="site-info">
+      <?php do_action( 'flint_credits' ); ?>
+      Proudly powered by <a href="http://wordpress.org/" rel="generator" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a> and the <a href="http://sparks.starverte.com/" rel="generator" title="<?php esc_attr_e( 'A WordPress Development Framework', 'flint' ); ?>">Sparks Framework</a>
+      <span class="sep"> | </span>
+      <?php $theme = wp_get_theme(); printf( __( 'Theme: %1$s by %2$s.', 'flint' ), $theme, '<a href="'.$theme->{'Author URI'}.'" rel="designer">'.$theme->{'Author'}.'</a>' ); ?>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
